96b4347dda7db718ab4961955c6676de13b5d035
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 01:26:55 +0800

    tukar nama

--- Aplikasi/Fail/Papar/index/03-organization.php
+++ Aplikasi/Fail/Papar/index/03-organization.php
@@ -1,99 +1,31 @@
-<!--A Design by W3layouts
-Author: W3layout
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Booth Convocation Management System</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Events Venue Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- bootstrap-css -->
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-<!--// bootstrap-css -->
-<!-- css -->
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-<!--// css -->
-<!-- font-awesome icons -->
-<link href="css/font-awesome.css" rel="stylesheet"> 
-<!-- //font-awesome icons -->
-<!-- font -->
-<link href='//fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
-<!-- //font -->
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$(".scroll").click(function(event){		
-			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-		});
-	});
-</script> 
-</head>
-<body>
-	<!-- banner -->
-	<div class="banner">
-		<div class="header">
-			<div class="container">
-				<div class="header-left">
-					<div class="w3layouts-logo">
-						<h1>
-							<a href="index.html">Booth <span>Convocation</span></a>
-						</h1>
-					</div>
+<?php
+//include('config.php');
+$url = URL . 'sumber/rangka-dawai/w3-event-venue/';
+include 'atasbawah/diatas.php';
+echo "\n";
+?>
+<!-- banner -->
+<div class="banner">
+<!-- start header -------------------------------------------------------------------------- -->
+	<div class="header">
+		<div class="container">
+			<div class="header-left">
+				<div class="w3layouts-logo">
+					<h1><a href="index.php">Booth <span>Convocation</span></a></h1>
 				</div>
-				<div class="top-nav">
-					<nav class="navbar navbar-default">
-							<div class="navbar-header">
-								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-									<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-							</div>
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								<li><a class="active" href="index.html">Home</a></li>
-								<li class=""><a href="#" class="dropdown-toggle hvr-bounce-to-bottom" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li><a class="hvr-bounce-to-bottom" href="aboutbooth.html">About Booth</a></li>
-										<li><a class="hvr-bounce-to-bottom" href="organization.html">Organization</a></li>          
-									</ul>
-								</li>
-								<li class=""><a href="#" class="dropdown-toggle hvr-bounce-to-bottom" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Exhibitors<span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li><a class="hvr-bounce-to-bottom" href="registeration.html">Registeration</a></li>
-										<li><a class="hvr-bounce-to-bottom" href="boothfees.html">Booth Fees</a></li>
-										<li><a class="hvr-bounce-to-bottom" href="floorplan.html">Floor Plan</a></li>
-										<li><a class="hvr-bounce-to-bottom" href="rulesregulations.html">Rules & Regulations</a></li>
-									</ul>
-								</li>	
-															
-								<li><a href="gallery.html">Gallery</a></li>
-								<li><a href="contact.html">Contact</a></li>
-								<li><a href="login.html">LOGIN</a></li>
-							</ul>
-							<div class="clearfix"> </div>
-						</div>	
-					</nav>		
-				</div>
-				<div class="clearfix"> </div>
 			</div>
-		</div>
-		<div class="agileinfo-top-heading">
-			<h2>Typography</h2>
+			<div class="top-nav"><?php 
+echo "\n"; include 'atasbawah/menubar.php'; echo "\n"; ?></div>
+			<div class="clearfix"> </div>
 		</div>
 	</div>
-	<!-- //banner -->
+<!-- end header -------------------------------------------------------------------------- -->
+<!-- start w3layouts-banner-slider-------------------------------------------------------- -->
+	<div class="agileinfo-top-heading">
+		<h2>Typography</h2>
+	</div>
+</div><!-- //banner -->
+
 	<!-- typography -->
 	<div class="typography">
 				<div class="container">
@@ -668,82 +600,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	</div>
 	<!-- //typography -->
 	<!-- footer -->
-	<footer>
-		<div class="agileits-w3layouts-footer-top">
-			<div class="container">
-				<div class="col-md-6 agileits-w3layouts-footer-top-left">
-					<p><i class="fa fa-phone" aria-hidden="true"></i> +1 234 567 8901</p>
-				</div>
-				<div class="col-md-6 agileits-w3layouts-footer-top-left">
-					<p><i class="fa fa-envelope" aria-hidden="true"></i> Email :<a href="mailto:example@email.com"> mail@example.com</a></p>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-		<div class="agileits-footer-bottom">
-			<div class="container">
-				<div class="agileits-footer-bottom-grids">
-					<div class="col-md-6 footer-bottom-left">
-						<h5>About Us</h5>
-						<div class="footer-img-grids">
-							<div class="footer-img">
-								<img src="images/f1.jpg" alt="" />
-							</div>
-							<div class="footer-img-info">
-								<p>Suspendisse potenti. Pellentesque pulvinar tellus at est ullamcorper, at elementum nibh laoreet. Nunc id diam in nulla sollicitudin auctor. Donec elementum felis turpis, vel interdum libero congue non. Mauris non magna convallis</p>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-					</div>
-					<div class="col-md-3 footer-bottom-right">
-						<h5>Newsletter</h5>
-						<p>Maecenas volutpat arcu at massa lacinia, ut interdum lorem venenatis.</p>
-						<form action="#" method="post">
-							<input type="text" placeholder="Subscribe" name="Subscribe" required="">
-							<input type="submit" value="Subscribe">
-						</form>
-					</div>
-					<div class="col-md-3 footer-bottom-right">
-						<h5>We are social</h5>
-						<div class="agileinfo-social-grids">
-							<ul>
-								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fa fa-rss"></i></a></li>
-								<li><a href="#"><i class="fa fa-vk"></i></a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
-		<div class="copyright">
-			<div class="container">
-				<p>© 2018 Booth Convocation Management System. All rights reserved | Design by Nur Iliya Liyana</a></p>
-			</div>
-		</div>
-	</footer>
-	<!-- //footer -->
-	<script src="js/SmoothScroll.min.js"></script>
-<script type="text/javascript" src="js/move-top.js"></script>
-<script type="text/javascript" src="js/easing.js"></script>
-	<!-- here stars scrolling icon -->
-	<script type="text/javascript">
-		$(document).ready(function() {
-			/*
-				var defaults = {
-				containerID: 'toTop', // fading element id
-				containerHoverID: 'toTopHover', // fading element hover id
-				scrollSpeed: 1200,
-				easingType: 'linear' 
-				};
-			*/
-								
-			$().UItoTop({ easingType: 'easeOutQuart' });
-								
-			});
-	</script>
-<!-- //here ends scrolling icon -->
-</body>	
-</html>
+<?php
+include 'atasbawah/footer.php'; echo "\n";
+include 'atasbawah/dibawah.php';
