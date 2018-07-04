e27114efc2443c347200a2bee6f34f0b15f9198a
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 01:26:31 +0800

    tukar nama

---
+++ Aplikasi/Fail/Papar/index/02-aboutbooth.php
@@ -0,0 +1,43 @@
+<?php
+//include('config.php');
+$url = URL . 'sumber/rangka-dawai/w3-event-venue/';
+include 'atasbawah/diatas.php';
+echo "\n";
+?>
+<!-- banner -->
+<div class="banner">
+<!-- start header -------------------------------------------------------------------------- -->
+	<div class="header">
+		<div class="container">
+			<div class="header-left">
+				<div class="w3layouts-logo">
+					<h1><a href="index.php">Booth <span>Convocation</span></a></h1>
+				</div>
+			</div>
+			<div class="top-nav"><?php 
+echo "\n"; include 'atasbawah/menubar.php'; echo "\n"; ?></div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- end header -------------------------------------------------------------------------- -->
+	<div class="agileinfo-top-heading">
+		<h2>ABOUT BOOTH</h2>
+	</div>
+</div>
+<!-- //banner -->
+<!-- typography -->
+<center><br><p>Welcome to the unisza booth convocation, the launch will be carried out by the cansellor.</p>
+<p>This convocation booth provides vendors or students with business opportunities to venture into business.</p>
+<br>
+<p>The unisza booth convocation will provide 4 types of product categories for sale and has a total of 
+200 booths will be provided by Unisza.</p>
+<br><br>
+<p><b>MAIN INFORMATION</b></p>
+<p>Performances by artist.</p>
+<p>Various activities will be provided.</p>
+<p>Cultural performances from multiple races.</p></center>
+<!-- //typography -->
+<!-- footer -->
+<?php
+include 'atasbawah/footer.php'; echo "\n";
+include 'atasbawah/dibawah.php';
