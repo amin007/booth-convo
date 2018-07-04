30e781a5ce28da50500d89711aa5924e4e75ea08
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-04 18:46:20 +0800

    pindah ke lokadi baru

---
+++ Aplikasi/Fail/Papar/index/backup/footer.php
@@ -0,0 +1,7 @@
+<footer>
+		<div class="copyright">
+			<div class="container">
+				<p>© 2018 Booth Convocation Management System. All rights reserved | Design by Nur Iliya Liyana</a></p>
+			</div>
+		</div>
+	</footer>
