ede29b154a22907bdcdefd08a0783f6874139ca4
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 07:47:00 +0800

    alih kedudukan form

--- Aplikasi/Fail/Papar/index/04-registeration-form.php
+++ Aplikasi/Fail/Papar/index/04-registeration-form.php
@@ -1,26 +1,24 @@
-<form id="borang_pengesahan_pembayaran" name="borang_pengesahan_pembayaran" 
-method="post" class="borang" style="padding-top:20px">
-<fieldset>
-	<table border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
-	<tr>
-		<td><img src="<?php echo $url ?>images/kotak_01_01.png" width="23" height="21" alt="" /></td>
-		<td background="<?php echo $url ?>images/kotak_01_02.png">&nbsp;</td>
-		<td><img src="<?php echo $url ?>images/kotak_01_03.png" width="31" height="21" alt="" /></td>
-	</tr>
-	<tr>
-		<td background="<?php echo $url ?>images/kotak_01_04.png">&nbsp;</td>
-		<td valign="top" bgcolor="#ffffff">
-<!-- start - booking table ----------------------------------------------------------------------------------------- -->
+<table border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+<tr>
+	<td><img src="<?php echo $url ?>images/kotak_01_01.png" width="23" height="21" alt="" /></td>
+	<td background="<?php echo $url ?>images/kotak_01_02.png">&nbsp;</td>
+	<td><img src="<?php echo $url ?>images/kotak_01_03.png" width="31" height="21" alt="" /></td>
+</tr>
+<tr>
+	<td background="<?php echo $url ?>images/kotak_01_04.png">&nbsp;</td>
+	<td valign="top" bgcolor="#ffffff">
+<!-- start - booking table --------------------------------------------------------------------------------------- -->
+	<form id="borang_pengesahan_pembayaran" name="borang_pengesahan_pembayaran"
+	method="post" class="borang" style="padding-top:20px">	<fieldset>
 <?php echo "\n"; include '04-registeration-table.php'; echo "\n"; ?>
+	</fieldset></form>
 <!-- end - booking table ----------------------------------------------------------------------------------------- -->
-		</td>
-		<td background="<?php echo $url ?>images/kotak_01_05.png">&nbsp;</td>
-	</tr>
-	<tr>
-		<td><img src="<?php echo $url ?>images/kotak_01_06.png" width="23" height="33" alt="" /></td>
-		<td background="<?php echo $url ?>images/kotak_01_07.png">&nbsp;</td>
-		<td height="33"><img src="<?php echo $url ?>images/kotak_01_08.png" width="31" height="33" alt="" /></td>
-	</tr>
-	</table>
-</fieldset>
-</form>
+	</td>
+	<td background="<?php echo $url ?>images/kotak_01_05.png">&nbsp;</td>
+</tr>
+<tr>
+	<td><img src="<?php echo $url ?>images/kotak_01_06.png" width="23" height="33" alt="" /></td>
+	<td background="<?php echo $url ?>images/kotak_01_07.png">&nbsp;</td>
+	<td height="33"><img src="<?php echo $url ?>images/kotak_01_08.png" width="31" height="33" alt="" /></td>
+</tr>
+</table>
