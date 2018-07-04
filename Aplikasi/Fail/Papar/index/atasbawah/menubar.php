73abde49e7c48842d59d0abf4dc4f13f5aaf879f
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-04 22:49:38 +0800

    buat manubar

---
+++ Aplikasi/Fail/Papar/index/atasbawah/menubar.php
@@ -0,0 +1,42 @@
+<!-- start nav -------------------------------------------------------------------------- -->
+<nav class="navbar navbar-default">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+		data-target="#bs-example-navbar-collapse-1">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+		</button>
+	</div>
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav">
+		<li><a class="active" href="index.php">Home</a></li>
+		<li class=""><a href="#" class="dropdown-toggle hvr-bounce-to-bottom" data-toggle="dropdown"
+		role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+			<li><a class="hvr-bounce-to-bottom" href="aboutbooth.php">About Booth</a></li>
+			<li><a class="hvr-bounce-to-bottom" href="#">Organization</a></li>
+			</ul>
+		</li>
+		<li class=""><a href="#" class="dropdown-toggle hvr-bounce-to-bottom" data-toggle="dropdown" 
+		role="button" aria-haspopup="true" aria-expanded="false">Exhibitors<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+			<li><a class="hvr-bounce-to-bottom" href="vendor/booking.php">Registeration</a></li>
+			<li><a class="hvr-bounce-to-bottom" href="floorplan.php">Floor Plan</a></li>
+			<li><a class="hvr-bounce-to-bottom" href="rulesregulations.php">Rules & Regulations</a></li>
+			</ul>
+		</li>
+		<li><a href="gallery.php">Gallery</a></li>
+		<li><a href="contact.php">Contact</a></li>
+		<li class=""><a href="#" class="dropdown-toggle hvr-bounce-to-bottom" data-toggle="dropdown" 
+		role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+			<li><a class="hvr-bounce-to-bottom" href="login.php"><i class="icon-user"></i>Sign In</a></li>
+			<li><a class="hvr-bounce-to-bottom" href="registeration.php">Sign Up</a></li>
+			</ul>
+		</li>
+		</ul>
+		<div class="clearfix"> </div>
+	</div>	
+</nav>
+<!-- end nav -------------------------------------------------------------------------- -->
