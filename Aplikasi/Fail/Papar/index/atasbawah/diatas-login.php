f92fa902806be7dbf67420213ca6d690be4f62c8
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 01:21:15 +0800

    buat versi fail yang berbeza

---
+++ Aplikasi/Fail/Papar/index/atasbawah/diatas-login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>css/style1.css" />
+<link href="<?php echo $url ?>SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $url ?>SpryAssets/SpryValidationSelect.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $url ?>SpryAssets/SpryValidationPassword.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $url ?>SpryAssets/SpryValidationConfirm.css" rel="stylesheet" type="text/css" />
+<script src="<?php echo $url ?>SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationSelect.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationPassword.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationConfirm.js" type="text/javascript"></script>
+</head>
---
+++ Aplikasi/Fail/Papar/index/atasbawah/diatas0.php
@@ -0,0 +1,49 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Booth Convocation Management System</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="<?php echo $url ?>css/style.css" />
+<link href="<?php echo $url ?>SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $url ?>SpryAssets/SpryValidationPassword.css" rel="stylesheet" type="text/css" />
+<script src="<?php echo $url ?>SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationPassword.js" type="text/javascript"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Events Venue Responsive web template, Bootstrap Web Templates, Flat Web Templates, 
+Android Compatible web template, Smartphone Compatible web template, free webdesigns for 
+Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> 
+addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); 
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstrap-css -->
+<link href="<?php echo $url ?>css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstrap-css -->
+<!-- css -->
+<link rel="stylesheet" href="<?php echo $url ?>css/style.css" type="text/css" media="all" />
+<!--// css -->
+<!-- font-awesome icons -->
+<link href="<?php echo $url ?>css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- font -->
+<link href='//fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+<!-- //font -->
+<script src="<?php echo $url ?>js/jquery-1.11.1.min.js"></script>
+<script src="<?php echo $url ?>js/bootstrap.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script> 
+</head>
+<body>
