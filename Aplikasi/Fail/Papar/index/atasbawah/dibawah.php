92e04b5173c27319a94577a10840ba6642411da9
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 19:14:51 +0800

    pindahkan javascript dari diatas.php ke dibawah.php

--- Aplikasi/Fail/Papar/index/atasbawah/dibawah.php
+++ Aplikasi/Fail/Papar/index/atasbawah/dibawah.php
@@ -1,52 +1,17 @@
-<!-- //footer -->
-<script src="<?php echo $url ?>js/responsiveslides.min.js"></script>
-<script>
-// You can also use "$(window).load(function() {"
-$(function () {// Slideshow 4
-	$("#slider4").responsiveSlides({
-		auto: true,
-		pager:true,
-		nav:false,
-		speed: 500,
-		namespace: "callbacks",
-		before: function () {
-		  $('.events').append("<li>before event fired.</li>");
-		},
-		after: function () {
-		  $('.events').append("<li>after event fired.</li>");
-		}
+<!-- untuk javaskrip sahaja bawah ini_get
+///////////////////////////////////////////////////////////////////// -->
+<script type="application/x-javascript">
+addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="<?php echo $url ?>js/jquery-1.11.1.min.js"></script>
+<script src="<?php echo $url ?>js/bootstrap.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
 	});
 });
 </script>
-<!--banner Slider starts Here-->
-<script src="<?php echo $url ?>js/responsiveslides.min.js"></script>
-<script src="<?php echo $url ?>js/jarallax.js"></script>
-<script src="<?php echo $url ?>js/SmoothScroll.min.js"></script>
-<script type="text/javascript">
-/* init Jarallax */
-$('.jarallax').jarallax({
-	speed: 0.5,
-	imgWidth: 1366,
-	imgHeight: 768
-	})
-</script>
-<script type="text/javascript" src="<?php echo $url ?>js/move-top.js"></script>
-<script type="text/javascript" src="<?php echo $url ?>js/easing.js"></script>
-<!-- here stars scrolling icon -->
-<script type="text/javascript">
-$(document).ready(function() {
-/*
-	var defaults = {
-	containerID: 'toTop', // fading element id
-	containerHoverID: 'toTopHover', // fading element hover id
-	scrollSpeed: 1200,
-	easingType: 'linear' 
-};
-*/							
-$().UItoTop({ easingType: 'easeOutQuart' });
-
-});
-</script>
-<!-- //here ends scrolling icon -->
 </body>	
 </html>
