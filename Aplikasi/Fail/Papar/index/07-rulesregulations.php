ae842a5336e8fbb207ac71ad84ad851470e57d3f
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 08:43:05 +0800

    tukar nama fail

---
+++ Aplikasi/Fail/Papar/index/07-rulesregulations.php
@@ -0,0 +1,15 @@
+<div class="agileinfo-top-heading">
+	<h2>RULES AND REGULATIONS</h2>
+</div>
+
+<!-- icons -->
+<br><center><p>Registeration fee process of RM50 is applicable for any category product.</p>
+<p>In the event of a cancellation or not eligible to participate for a whatever reason, the registeration fee process is non refundable.</p>
+<br><p><b>Terms Of Payment</b><p>
+<p>i. Booking registeration must be made together with full payment when the status is proceed with our organiser.</p>
+<p>ii. Full payment consists of the following:</p>
+<p>   a.Booth Rental</p>
+<p>   b.Refuncable Security Deposit</p>
+<p>ii. All registeration made with full payment are subject to final acceptance by the Organiser.<p>
+<p>iii. Payment shall be refunded in the event booking registeration is rejected.</p>
+<!-- //icons -->
