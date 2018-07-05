b1816741bad8bda81bf51b33a2beeea798317f3c
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 08:47:18 +0800

    susun balik koding html

--- Aplikasi/Fail/Papar/index/07-rulesregulations.php
+++ Aplikasi/Fail/Papar/index/07-rulesregulations.php
@@ -1,15 +1,49 @@
-<div class="agileinfo-top-heading">
-	<h2>RULES AND REGULATIONS</h2>
-</div>
+<?php
+//include('config.php');
+$url = URL . 'sumber/rangka-dawai/w3-event-venue/';
+include 'atasbawah/diatas.php';
+echo "\n";
+?>
+<!-- banner -->
+<div class="banner">
+<!-- start header -------------------------------------------------------------------------- -->
+	<div class="header">
+		<div class="container">
+			<div class="header-left">
+				<div class="w3layouts-logo">
+					<h1><a href="index.php">Booth <span>Convocation</span></a></h1>
+				</div>
+			</div>
+			<div class="top-nav"><?php
+echo "\n"; include 'atasbawah/menubar.php'; echo "\n"; ?></div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- end header -------------------------------------------------------------------------- -->
+<!-- start agileinfo-top-heading-------------------------------------------------------- -->
+	<div class="agileinfo-top-heading">
+		<h2>RULES AND REGULATIONS</h2>
+	</div>
+</div><!-- //banner -->
 
-<!-- icons -->
-<br><center><p>Registeration fee process of RM50 is applicable for any category product.</p>
-<p>In the event of a cancellation or not eligible to participate for a whatever reason, the registeration fee process is non refundable.</p>
-<br><p><b>Terms Of Payment</b><p>
-<p>i. Booking registeration must be made together with full payment when the status is proceed with our organiser.</p>
+<!-- start - section body ----------------------------------------------------------------------- -->
+<!-- icons --><br><center>
+<p>Registeration fee process of RM50 is applicable for any category product.</p>
+<p>In the event of a cancellation or not eligible to participate for a whatever reason,
+the registeration fee process is non refundable.</p>
+<br>
+<p><b>Terms Of Payment</b><p>
+<p>i. Booking registeration must be made together with full payment when the status is
+proceed with our organiser.</p>
 <p>ii. Full payment consists of the following:</p>
 <p>   a.Booth Rental</p>
 <p>   b.Refuncable Security Deposit</p>
 <p>ii. All registeration made with full payment are subject to final acceptance by the Organiser.<p>
 <p>iii. Payment shall be refunded in the event booking registeration is rejected.</p>
-<!-- //icons -->
+</center><!-- //icons -->
+<!-- ended - section body ----------------------------------------------------------------------- -->
+
+<!-- footer -->
+<?php
+include 'atasbawah/footer.php'; echo "\n";
+include 'atasbawah/dibawah.php';
