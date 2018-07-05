bd9da08553431d6bf80bc17a2292ba90d61828d6
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 19:01:27 +0800

    susun balik koding

--- Aplikasi/Fail/Papar/index/atasbawah/footer.php
+++ Aplikasi/Fail/Papar/index/atasbawah/footer.php
@@ -1,7 +1,5 @@
 <footer class="navbar-fixed-bottom">
-	<div class="copyright">
-		<div class="container">
-			<p>© 2018 Booth Convocation Management System. All rights reserved | Design by Nur Iliya Liyana</a></p>
-		</div>
-	</div>
+<div class="copyright"><div class="container">
+	<p>&copy; 2018 Booth Convocation Management System. All rights reserved | Design by Nur Iliya Liyana</p>
+</div></div>
 </footer>
