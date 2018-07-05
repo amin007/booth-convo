9098db7839bf9cc82f4bc4da545d0617b14b7b57
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-05 19:13:46 +0800

    pindahkan javascript ke dibawah.php hanya paparkan css dalam diatas.php

--- Aplikasi/Fail/Papar/index/atasbawah/diatas.php
+++ Aplikasi/Fail/Papar/index/atasbawah/diatas.php
@@ -14,9 +14,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <meta name="keywords" content="Events Venue Responsive web template, Bootstrap Web Templates, Flat Web Templates, 
 Android Compatible web template, Smartphone Compatible web template, free webdesigns for 
 Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> 
-addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); 
-function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- bootstrap-css -->
 <link href="<?php echo $url ?>css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <!--// bootstrap-css -->
@@ -30,15 +27,5 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href='//fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
 <!-- //font -->
-<script src="<?php echo $url ?>js/jquery-1.11.1.min.js"></script>
-<script src="<?php echo $url ?>js/bootstrap.js"></script>
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$(".scroll").click(function(event){		
-			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-		});
-	});
-</script> 
 </head>
 <body>
