e0e571b065a939c3111124bba79d5bde1af2371f
Amin007 Ledang <helmi007@yahoo.com>
2018-07-08 17:18:34 +0800

    tambah class admin_tanya

---
+++ Aplikasi/Kelas/Utama/Tanya/admin_tanya.php
@@ -0,0 +1,46 @@
+<?php
+namespace Aplikasi\Tanya; //echo __NAMESPACE__; 
+class Admin_Tanya extends \Aplikasi\Kitab\Tanya
+{
+#=====================================================================================================
+	public function __construct()
+	{
+		parent::__construct();
+	}
+#---------------------------------------------------------------------------------------------------#
+	function data_contoh($pilih)
+	{
+		$data = array(
+			'namaPendek' => 'james007',
+			'namaPenuh' => 'Polan Bin Polan',
+			'level' => 'pelawat'
+		); # dapatkan medan terlibat
+		$kira = 1; # kira jumlah data
+
+		return ($pilih==1) ? $kira : $data; # pulangkan nilai
+	}
+#---------------------------------------------------------------------------------------------------#
+	public function susunPembolehubah()
+	{
+		list($myTable, $medan01, $medan02, $medan) = dpt_senarai('jadual_login');
+		$carian = $susun = null;
+		# semak database
+			$carian[] = array('fix'=>'xlike', # cari x= atau %like%
+				'atau'=>'WHERE', # WHERE / OR / AND
+				'medan' => 'username', # cari dalam medan apa
+				'apa' => 'admin'); # benda yang dicari
+			$carian[] = array('fix'=>'like', # cari x= atau %like%
+				'atau'=>'AND', # WHERE / OR / AND
+				'medan' => 'level', # cari dalam medan apa
+				'apa' => 'admin2'); # benda yang dicari//*/
+
+		return array($myTable, $medan, $carian, $susun); # pulangkan nilai
+	}
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#=====================================================================================================
+}
