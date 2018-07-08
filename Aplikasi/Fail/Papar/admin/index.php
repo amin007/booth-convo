a3fd92ff7a579a9a78e2cf052d2f784bbced089a
Amin007 Ledang <helmi007@yahoo.com>
2018-07-08 13:46:02 +0800

    tambah fail index untuk admin

---
+++ Aplikasi/Fail/Papar/admin/index.php
@@ -0,0 +1,19 @@
+<div class="container">
+<hr><h1>Admin - Kita Tanya Apa Khabar</h1><hr>
+<div class="hero-unit">
+<p><?php
+$Sesi = new \Aplikasi\Kitab\Sesi();
+$Sesi->init();
+//echo '<pre>'; print_r($_SESSION) . '</pre>';
+echo 'namaPendek=' . $Sesi->get('namaPendek') . '<br>';
+echo 'namaPenuh=' . $Sesi->get('namaPenuh') . '<br>';
+echo 'email=' . $Sesi->get('email') . '<br>';
+echo 'levelPengguna=' . $Sesi->get('levelPengguna') . '';//*/
+?></p>
+
+	<a class="btn btn-primary btn-large" href="<?php echo URL ?>admin/logout">Pergi Lebih Jauh<i class="fa fa-binoculars fa-2x"></i></a>
+
+</div><!-- / class="hero-unit" -->
+</div><!-- / class="container" -->
+
+<?php //echo semakDataSesi(); ?>
