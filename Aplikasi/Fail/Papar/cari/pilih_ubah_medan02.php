babc8c563ee63b573d0dfcac87d63a8b23309e90
Amin007 Ledang <helmi007@yahoo.com>
2018-07-09 08:51:07 +0800

    tukar format untuk bootstrap 4.1.1

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
@@ -2,16 +2,16 @@
 $html = new Aplikasi\Kitab\Borang02_Ubah;
 $aksi = URL . $this->_method . '/ubahSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
-$class2 = 'col-sm-6 '; # untuk $data
+$class2 = 'col-sm-6'; # untuk $data
 $html->medanCarian(
 	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
 );?>
-<form method="POST" action="<?php echo $aksi ?>"
-class="form-horizontal"><?php echo "\n";
+<div class="jumbotron">
+<form method="POST" action="<?php echo $aksi ?>"><?php echo "\n";
 for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
-		?><div class="form-group"><?php echo "\n\t";
+		?><div class="form-group row"><?php echo "\n\t";
 		?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
 		?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
@@ -19,8 +19,9 @@ for ($kira=0; $kira < count($row); $kira++)
 			$kira, $key, $data);
 		echo $paparData . "\n\t";
 		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
-		?></div><!-- / class="form-group" --><?php echo "\n";
+		?></div><!-- / class="form-group row" --><?php echo "\n";
 	}## --------------------------------------------------------------------------
 }$html->medanHantar($this->_jadual, $class1);
 /*<!-- / class="container" -->*/ ?>
 </form><!-- / class="form-horizontal" -->
+</div><!-- / class="container" -->
