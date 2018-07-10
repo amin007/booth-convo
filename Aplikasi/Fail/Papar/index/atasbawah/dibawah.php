dac1dae80464a5eb49189ce58ec7db55196198fb
Amin007 Ledang <helmi007@yahoo.com>
2018-07-10 09:21:33 +0800

    tambah responsiveslides.min.js dan jzBox.js

--- Aplikasi/Fail/Papar/index/atasbawah/dibawah.php
+++ Aplikasi/Fail/Papar/index/atasbawah/dibawah.php
@@ -13,5 +13,20 @@ jQuery(document).ready(function($) {
 	});
 });
 </script>
+<script src="<?php echo $url ?>js/responsiveslides.min.js"></script>
+<script>// You can also use "$(window).load(function() {"
+$(function () {// Slideshow 4
+	$("#slider4").responsiveSlides({
+		auto: true,
+		pager:true,
+		nav:false,
+		speed: 500,
+		namespace: "callbacks",
+		before: function () { $('.events').append("<li>before event fired.</li>"); },
+		after:  function () { $('.events').append("<li>after event fired. </li>"); }
+	});
+});
+</script>
+<script src="<?php echo $url ?>js/jzBox.js"></script>
 </body>	
 </html>
