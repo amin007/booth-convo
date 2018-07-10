49ff74ce6378a26c25d8451177018ef756da42fa
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-10 19:13:52 +0800

    tukar admin kepada admin 2

--- Aplikasi/Fail/Papar/admin2/index.php
+++ Aplikasi/Fail/Papar/admin2/index.php
@@ -1,5 +1,5 @@
 <div class="container">
-<hr><h1>Admin2 - Kita Tanya Apa Khabar</h1><hr>
+<hr><h1>Admin - Kita Tanya Apa Khabar</h1><hr>
 <div class="hero-unit">
 <p><?php
 $Sesi = new \Aplikasi\Kitab\Sesi();
@@ -11,7 +11,7 @@ echo 'email=' . $Sesi->get('email') . '<br>';
 echo 'levelPengguna=' . $Sesi->get('levelPengguna') . '';//*/
 ?></p>
 
-	<a class="btn btn-primary btn-large" href="<?php echo URL ?>admin2/logout">Pergi Lebih Jauh<i class="fa fa-binoculars fa-2x"></i></a>
+	<a class="btn btn-primary btn-large" href="<?php echo URL ?>admin/logout">Pergi Lebih Jauh<i class="fa fa-binoculars fa-2x"></i></a>
 
 </div><!-- / class="hero-unit" -->
 </div><!-- / class="container" -->
