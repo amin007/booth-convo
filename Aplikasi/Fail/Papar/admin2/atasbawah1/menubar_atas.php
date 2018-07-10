f7acc027399c2f674d1ce589426e3a6755c28859
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-10 19:35:43 +0800

    tukar lokasi gambar

--- Aplikasi/Fail/Papar/admin2/atasbawah1/menubar_atas.php
+++ Aplikasi/Fail/Papar/admin2/atasbawah1/menubar_atas.php
@@ -13,7 +13,7 @@ $iconFA['Power'] = '<i class="fas fa-power-off"></i>';
 		<a href="#" class="btn btn-link sidebar-mobile-toggle d-md-none mr-auto">
 			<i class="fa fa-bars"></i>
 		</a>
-		<a class="navbar-brand" href="#"><img src="./imgs/logo.png" alt="logo"></a>
+		<a class="navbar-brand" href="#"><img src="<?php echo $url ?>imgs/logo.png" alt="logo"></a>
 		<a href="#" class="btn btn-link sidebar-toggle d-md-down-none">
 			<i class="fa fa-bars"></i>
 		</a>
@@ -26,7 +26,7 @@ $iconFA['Power'] = '<i class="fas fa-power-off"></i>';
 		</li>
 		<li class="nav-item dropdown">
 			<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				<img src="./imgs/avatar-1.png" class="avatar avatar-sm" alt="logo">
+				<img src="<?php echo $url ?>imgs/avatar-1.png" class="avatar avatar-sm" alt="logo">
 				<span class="small ml-1 d-md-down-none"><?php echo ucwords($userName) . " ($userID)";?></span>
 			</a>
 			<div class="dropdown-menu dropdown-menu-right">
