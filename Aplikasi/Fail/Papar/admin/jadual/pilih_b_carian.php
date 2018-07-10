5fa5d159ec23ba49634d58d3624e82186bf8e877
Amin007 Ledang <helmi007@yahoo.com>
2018-07-10 13:34:32 +0800

    tukar kod css bagi bootstrap 4.1.1

--- Aplikasi/Fail/Papar/admin/jadual/pilih_b_carian.php
+++ Aplikasi/Fail/Papar/admin/jadual/pilih_b_carian.php
@@ -6,13 +6,13 @@ $butangHantar = 'Search';
 	<div align="center"><form method="GET" action="<?=$mencari?>"
 	class="form-inline" autocomplete="off">
 	<?php //echo $mencari . '<br>' . "\r" ?>
-	<div class="form-group"><div class="input-group">
+	<div class="form-group row"><div class="input-group">
 		<input type="text" name="cari" class="form-control" autofocus
 		placeholder="Search : "
 		id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
-		<span class="input-group-addon">
-		<input type="submit" value="<?=$butangHantar?>">
-		</span>
+		<div class="input-group-append"><span class="input-group-text" id="basic-addon2">
+			<input type="submit" value="<?=$butangHantar?>">
+		</span></div><!-- / class="input-group-append" -->
 	</div></div>
 	<div class="suggestionsBox" id="suggestions" style="display: none; " >
 		<div class="suggestionList" id="autoSuggestionsList">&nbsp;</div>
