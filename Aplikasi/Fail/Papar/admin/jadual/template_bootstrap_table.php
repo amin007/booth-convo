be6b315495f6c76b97cdccca15b44942d19980df
Amin007 Ledang <helmi007@yahoo.com>
2018-07-10 16:56:32 +0800

    include fail berdasarkan $this->_pilih

--- Aplikasi/Fail/Papar/admin/jadual/template_bootstrap_table.php
+++ Aplikasi/Fail/Papar/admin/jadual/template_bootstrap_table.php
@@ -8,10 +8,11 @@ foreach ($this->senarai as $myTable => $row)
 		$tajukjadual = 'Jadual : ' . $myTable; ?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->
 <?php 
-include 'pilih_a_tajuk.php';
-include 'pilih_c_muatnaik.php';
-include 'pilih_b_carian.php';
-include 'pilih_' . $pilihJadual . '.php'; 
+if($this->_pilih != 'report') include 'pilih_a_tajuk.php';
+if($this->_pilih == 'login') include 'pilih_c_muatnaik.php';
+if($this->_pilih == 'product') include 'pilih_b_carian.php';
+if($this->_pilih != 'report') include 'pilih_' . $pilihJadual . '.php';
+if($this->_pilih == 'report') include 'z_template-graf.php';
 ?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->
 <?php
