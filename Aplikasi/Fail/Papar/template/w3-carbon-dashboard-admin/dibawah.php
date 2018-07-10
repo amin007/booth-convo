71ed79bca7a3bc13dcd48f544bf1dc9825778b7f
Amin007 Ledang <helmi007@yahoo.com>
2018-07-10 10:49:54 +0800

    buat template untuk w3-carbon-dashboard-admin

---
+++ Aplikasi/Fail/Papar/template/w3-carbon-dashboard-admin/dibawah.php
@@ -0,0 +1,22 @@
+<?php echo "\n"; ?>
+<!-- tamat template 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+</div><!-- / class="content" --
+</div><!-- / class="main-container" -->
+</div><!-- / class="page-wrapper" --> 
+
+<?php
+/*
+<script src="<?php echo $url ?>SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationSelect.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationPassword.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>SpryAssets/SpryValidationConfirm.js" type="text/javascript"></script>
+*/ ?>
+<script src="<?php echo $url ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo $url ?>vendor/popper.js/popper.min.js"></script>
+<script src="<?php echo $url ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo $url ?>vendor/chart.js/chart.min.js"></script>
+<script src="<?php echo $url ?>js/carbon.js"></script>
+<script src="<?php echo $url ?>js/demo.js"></script>
+</body>
+</html>
