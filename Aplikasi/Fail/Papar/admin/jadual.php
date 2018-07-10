78ac89a1d9b5898dbad32bf718ae43386689b381
Amin007 Ledang <helmi007@yahoo.com>
2018-07-10 16:55:31 +0800

    tutup komen

--- Aplikasi/Fail/Papar/admin/jadual.php
+++ Aplikasi/Fail/Papar/admin/jadual.php
@@ -22,6 +22,7 @@ include 'atasbawah1/menu_atas.php';
 <div class="row">
 <div class="col-md-12">
 <div class="card"><?php
+//include 'jadual/z_template-graf.php';
 //include 'z_template-login.php';
 //include 'z_template-admin.php';
 ?>
