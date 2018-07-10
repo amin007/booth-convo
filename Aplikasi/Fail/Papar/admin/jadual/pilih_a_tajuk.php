edf0806f7187902bce51a7f431c94d46a5687729
Amin007 Ledang <helmi007@yahoo.com>
2018-07-10 16:58:54 +0800

    pilih warna berdasarkan $this->_pilih

--- Aplikasi/Fail/Papar/admin/jadual/pilih_a_tajuk.php
+++ Aplikasi/Fail/Papar/admin/jadual/pilih_a_tajuk.php
@@ -1,5 +1,9 @@
-	<table width="750" cellpadding="3" cellspacing="1">
+<?php
+$subTajuk = ($this->_pilih == 'login') ?
+	'sub-tajuk-kuning2' : 'sub-tajuk-kelabu';
+?>
+	<table width="700" cellpadding="3" cellspacing="1">
 	<tr><td>
-		<div class="sub-tajuk-kelabu" align="center"><?php 
+		<div class="<?php echo $subTajuk ?>" align="center"><?php
 		echo $tajukjadual ?></div>
 	</td></tr></table>
