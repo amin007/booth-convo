8f9434f0d91abfc5e38f6b870614ab48bea18ead
Amin007 Ledang <helmi007@yahoo.com>
2018-07-11 08:55:33 +0800

    tukar table kepada class="row"

--- Aplikasi/Fail/Papar/admin/jadual/pilih_a_tajuk.php
+++ Aplikasi/Fail/Papar/admin/jadual/pilih_a_tajuk.php
@@ -2,8 +2,9 @@
 $subTajuk = ($this->_pilih == 'login') ?
 	'sub-tajuk-kuning2' : 'sub-tajuk-kelabu';
 ?>
-	<table width="700" cellpadding="3" cellspacing="1">
-	<tr><td>
-		<div class="<?php echo $subTajuk ?>" align="center"><?php
-		echo $tajukjadual ?></div>
-	</td></tr></table>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="<?php echo $subTajuk ?>" align="center"><?php
+			echo $tajukjadual ?></div>
+		</div><!-- / class="col-md-3" -->
+	</div><!-- / class="row" -->
