bc6c538f8f49dea75f875acce35b0b088005f2d2
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-12 00:05:18 +0800

    jika primari-key, usah papar medan yang berkaitan. biar DB urusan

--- Aplikasi/Fail/Papar/admin/b_baru/y_contoh-borang1.php
+++ Aplikasi/Fail/Papar/admin/b_baru/y_contoh-borang1.php
@@ -13,6 +13,8 @@ $class2 = 'col-sm-7'; # untuk $data
 foreach ( $this->medan as $key=>$data )
 {## papar data $row ----------------------------------------------------------
 	list($type,$pri) = explode('|', $data);
+	if($pri == 'PRI'): echo '';
+	else:
 	?><div class="form-group row"><?php echo "\n\t";
 	?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
 	?></label><?php echo "\n\t";
@@ -21,6 +23,7 @@ foreach ( $this->medan as $key=>$data )
 	echo $paparData . "\n\t";
 	?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
 	?></div><!-- / class="form-group row" --><?php echo "\n";
+	endif;
 }## --------------------------------------------------------------------------
 $html->medanHantar($this->_jadual, $class1);
 /*<!-- / class="container" -->*/ ?>
