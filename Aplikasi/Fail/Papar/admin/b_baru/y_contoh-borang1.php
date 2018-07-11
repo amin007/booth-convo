4964f1b1cfc47b3248e6a2111e343e3a0b6742b1
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-11 23:13:13 +0800

    susun balik pembolehubah yang baru, buang yang tiada kaitan

--- Aplikasi/Fail/Papar/admin/b_baru/y_contoh-borang1.php
+++ Aplikasi/Fail/Papar/admin/b_baru/y_contoh-borang1.php
@@ -2,31 +2,26 @@
 //https://getbootstrap.com/docs/4.1/components/input-group/
 //echo '<pre>$this->medan:'; print_r($this->medan); echo '</pre>';
 $html = new Aplikasi\Kitab\Borang01_Tambah;
-$tata = new Aplikasi\Kitab\Tatasusunan;
-$a = $tata->senaraiMedan($this->_pilih);
 //$aksi = URL . $this->_method . '/ubahSimpan/' . $this->carian[0];
 $aksi =  URL . '/borang/insertID/' . $this->_jadual;
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6'; # untuk $data
-$b = $this->_pilih;
-$kira = 0;
 //*/?>
 <div class="container">
 <h5><?php echo $aksi ?></h5>
 <form method="POST" action="<?php echo $aksi ?>"><?php echo "\n";
-//for ($kira=0; $kira < count($this->medan); $kira++)
-//{
-	foreach ( $this->medan as $key=>$data )
-	{## papar data $row ----------------------------------------------------------
-		?><div class="form-group row"><?php echo "\n\t";
-		?><label for="inputTajuk" class="col-sm-4 control-label"><?php echo $key
-		?></label><?php echo "\n\t";
-		?><div class="<?php echo $class2 ?>"><?php  echo "\n\t\t";
-		$paparData = $html->baruInput($a,$this->_jadual,$kira,$key,$data);
-		echo $paparData . "\n\t";
-		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
-		?></div><!-- / class="form-group row" --><?php echo "\n";
-	}## --------------------------------------------------------------------------
+foreach ( $this->medan as $key=>$data )
+{## papar data $row ----------------------------------------------------------
+	list($type,$pri) = explode('|', $data);
+	?><div class="form-group row"><?php echo "\n\t";
+	?><label for="inputTajuk" class="col-sm-4 control-label"><?php echo $key
+	?></label><?php echo "\n\t";
+	?><div class="<?php echo $class2 ?>"><?php  echo "\n\t\t";
+	$paparData = $html->baruInput($this->_jadual,$key,$data,$type,$pri);
+	echo $paparData . "\n\t";
+	?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
+	?></div><!-- / class="form-group row" --><?php echo "\n";
+}## --------------------------------------------------------------------------
 $html->medanHantar($this->_jadual, $class1);
 /*<!-- / class="container" -->*/ ?>
 </form><!-- / class="form-horizontal" -->
