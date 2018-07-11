791e1e405f059d1ab6c377d62b74c31cee0fe469
Amin007 Ledang <helmi007@yahoo.com>
2018-07-11 15:47:18 +0800

    buang $b dan $c, tambah $this_jadual, $kira

--- Aplikasi/Fail/Papar/admin/y_contoh-borang1.php
+++ Aplikasi/Fail/Papar/admin/y_contoh-borang1.php
@@ -8,8 +8,8 @@ $a = $tata->senaraiMedan($this->_pilih);
 $aksi =  URL . '/borang/insertID/' . $this->_jadual;
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6'; # untuk $data
-$c = null;
 $b = $this->_pilih;
+$kira = 0;
 //*/?>
 <div class="container">
 <?php echo $aksi ?>
@@ -22,7 +22,7 @@ $b = $this->_pilih;
 		?><label for="inputTajuk" class="col-sm-4 control-label"><?php echo $data
 		?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
-		$paparData = $html->ubahInput($a,$b,$c,$data,$data);
+		$paparData = $html->ubahInput($a,$this->_jadual,$kira,$data,$data);
 		echo $paparData . "\n\t";
 		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
 		?></div><!-- / class="form-group row" --><?php echo "\n";
