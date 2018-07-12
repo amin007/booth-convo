3e370fcb02782bb1fb7f63c8ef7b670c5f222e6e
Amin007 Ledang <helmi007@yahoo.com>
2018-07-12 16:20:26 +0800

    ubah warna latar belakang

--- Aplikasi/Fail/Papar/admin2/b_baru.php
+++ Aplikasi/Fail/Papar/admin2/b_baru.php
@@ -5,20 +5,20 @@ include 'atasbawah1/diatas.php';
 include 'atasbawah1/menu_atas.php';
 ?>
 
-<div class="content"><!-- mula template
+<div class="content" style="color:#000000; background-color: lightblue"><!-- mula template
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
 <div class="container-fluid">
 
 <div class="row">
 <div class="col-md-12">
-<div class="card"><?php
+<?php
 //include 'y_website-form.php';
 $tajukjadual = 'Daftar Baru';
 include 'jadual/pilih_a_tajuk.php';
 include 'b_baru/y_contoh-borang1.php';
 echo "\n\n";
 ?>
-</div><!-- / class="card" -->
+<!-- / class="card" -->
 </div><!-- / class="col-md-3" -->
 </div><!-- / class="row" -->
 
