077a65c703bc8de9806acb5f14ffcb8e526c410a
Amin007 Ledang <helmi007@yahoo.com>
2018-07-12 16:15:46 +0800

    tutup komen

--- Aplikasi/Kelas/Kitab/Vendor/Borang/Borang01_Tambah.php
+++ Aplikasi/Kelas/Kitab/Vendor/Borang/Borang01_Tambah.php
@@ -222,9 +222,9 @@ class Borang01_Tambah
 
 		return ''
 		. '<div class="input-group mb-3">'
-		. $tab3 . '<div class="input-group-prepend"><span class="input-group-text">'
-		. $tab3 . 'idUser = ' . $idUser . '-' . $namaPendek
-		. $tab3 . '</span></div>' . $tab3
+		//. $tab3 . '<div class="input-group-prepend"><span class="input-group-text">'
+		//. $tab3 . 'idUser = ' . $idUser . '-' . $namaPendek
+		//. $tab3 . '</span></div>' . $tab3
 		. '<input type="text" ' . $name . ' value="' . $data . '"'
 		. ' class="form-control" readonly>'
 		//. '<input type="hidden" ' . $name . ' value="' . $data . '">'
