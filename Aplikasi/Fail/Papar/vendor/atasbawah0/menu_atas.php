3d24d063e14e6dec98ebd58294d5cee82637aaf5
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 09:30:01 +0800

    tambah menu default untuk vendor

---
+++ Aplikasi/Fail/Papar/vendor/atasbawah0/menu_atas.php
@@ -0,0 +1,39 @@
+<!-- menu_atas.php A0 --><?php 
+$sesi = \Aplikasi\Kitab\Sesi::init();
+//echo '<pre>MENU_ATAS - $_SESSION:', print_r($_SESSION, 1) . '</pre><br>';
+# set pembolehubah
+$pengguna = \Aplikasi\Kitab\Sesi::get('namaPendek');
+$level = \Aplikasi\Kitab\Sesi::get('levelPengguna');
+
+$senaraiPengguna = array('pentadbir','biasa');
+$senaraiPentadbir = array('pentadbir','biasa');
+if (in_array($level, $senaraiPentadbir)) 
+	$paras = '' . $level;
+elseif (in_array($level, $senaraiPengguna))
+	$paras = '' . $level;
+else 
+	$paras = null; # untuk pelawat sahaja
+
+$iconFA['home2'] = '<i class="fa fa-home fa-2x" aria-hidden="true"></i>';
+$iconFA['video'] = '<i class="fas fa-video"></i>';
+$iconFA['search'] = '<i class="fas fa-search"></i>';
+
+echo "\n\n";
+if ($paras == null): else: ?>
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+	<a class="navbar-brand" href="<?php echo URL ?>">
+		<?php echo $iconFA['video'] . Tajuk_Muka_Surat . ':' . $paras ?></a>
+	<a class="navbar-brand" href="<?php echo URL ?>ruangtamu/logout">
+		<i class="fa fa-times fa-2x" aria-hidden="true"></i>Keluar</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<?php //include 'menubar_atas.php'; ?>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</nav>
+<?php
+endif;
---
+++ Aplikasi/Fail/Papar/vendor/atasbawah0/menubar_atas.php
@@ -0,0 +1,36 @@
+<?php 
+$nav = 'class="dropdown-toggle" data-toggle="dropdown"';
+//<ul class="nav navbar-nav navbar-right">
+$classUL = 'nav navbar-nav navbar-right';
+$iconFA['User'] = '<i class="fas fa-user"></i>';
+$iconFA['Barcode'] = '<i class="fas fa-barcode"></i>';
+$iconFA['Filter'] = '<i class="fas fa-filter"></i>';
+$iconFA['Stats'] = '<i class="fas fa-chart-bar"></i>';
+$iconFA['Ask'] = '<i class="fas fa-question-circle"></i>';
+$iconFA['Power'] = '<i class="fas fa-power-off"></i>';
+?>
+	<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+		<ul class="navbar-nav mr-auto">
+		<li class="nav-item">
+			<a class="nav-link" href="#">Link</a>
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
+			aria-haspopup="true" aria-expanded="false">Dropdown</a>
+			<div class="dropdown-menu" aria-labelledby="dropdown01">
+			<a class="dropdown-item" href="#">Action</a>
+			<a class="dropdown-item" href="#">Another action</a>
+			<a class="dropdown-item" href="#">Something else here</a>
+			</div>
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
+			aria-haspopup="true" aria-expanded="false">Dropdown</a>
+			<div class="dropdown-menu" aria-labelledby="dropdown01">
+			<a class="dropdown-item" href="#">Action</a>
+			<a class="dropdown-item" href="#">Another action</a>
+			<a class="dropdown-item" href="#">Something else here</a>
+			</div>
+		</li>
+		</ul>
+	</div>
