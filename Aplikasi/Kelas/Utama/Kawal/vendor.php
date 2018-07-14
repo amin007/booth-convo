c4f1ccb944a40941ee3f84b132dac7fa5bc54a7d
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 12:44:32 +0800

    tambah fungsi debugKandunganPaparan(), kandunganPaparan(), panggilDB(), tambahMedanDB()

--- Aplikasi/Kelas/Utama/Kawal/vendor.php
+++ Aplikasi/Kelas/Utama/Kawal/vendor.php
@@ -56,6 +56,66 @@ class Vendor extends \Aplikasi\Kitab\Kawal
 		//exit;
 	}
 #==========================================================================================
+#-------------------------------------------------------------------------------------------
+	function debugKandunganPaparan()
+	{
+		echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
+		echo '<pre>';
+		echo '<br>$this->papar->senarai : '; print_r($this->papar->senarai);
+		echo '<br>$this->papar->myTable : '; print_r($this->papar->myTable);
+		echo '<br>$this->papar->_jadual : '; print_r($this->papar->_jadual);
+		echo '<br>$this->papar->carian : '; print_r($this->papar->carian);
+		echo '<br>$this->papar->c1 : '; print_r($this->papar->c1);
+		echo '<br>$this->papar->c2 : '; print_r($this->papar->c2);
+		echo '<br>$this->papar->medan : '; print_r($this->papar->medan);
+		echo '<br>$this->papar->_pilih : '; print_r($this->papar->_pilih);
+		echo '<br>$this->papar->template : '; print_r($this->papar->template);
+		echo '</pre>';
+	}
+#-------------------------------------------------------------------------------------------
+	function kandunganPaparan($pilih, $myTable)
+	{
+		//$this->papar->senarai[$myTable] = null;
+		$this->papar->myTable = $myTable;
+		$this->papar->carian[] = 'semua';
+		$this->papar->c1 = $this->papar->c2 = null;
+		$this->papar->_pilih = $pilih;
+		//$this->papar->template = 'biasa';
+		$this->papar->template = 'bootstrap_table';
+		//$this->papar->template = 'bootstrap';
+		//$this->papar->template = 'khas01';
+		//*/
+	}
+#-------------------------------------------------------------------------------------------
+	function panggilDB($pilih)
+	{
+		# Set pembolehubah utama
+		list($myTable, $medan, $carian, $susun) = $this->tanya->susunPembolehubah($pilih);
+		$this->papar->senarai[$myTable] = $this->tanya->//cariSql
+			cariSemuaData
+			($myTable, $medan, $carian, $susun);
+		if( count($this->papar->senarai[$myTable]) == 0 ):
+			//echo 'jumlah $senarai kosong';
+			$this->papar->senarai[$myTable] = null;
+		endif;//*/
+		# Set pembolehubah untuk Papar
+		$this->kandunganPaparan($pilih, $myTable);
+		//$this->debugKandunganPaparan($pilih, $myTable);
+	}
+#-------------------------------------------------------------------------------------------
+	function tambahMedanDB($pilih)
+	{
+		//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
+		list($myTable) = $this->tanya->tambahPembolehubah($pilih);
+		$this->papar->medan = $this->tanya->//paparMedan
+			//paparMedan02 //pilihMedan //pilihMedan02
+			pilihMedan02($myTable);//*/
+
+		# Set pembolehubah untuk Papar
+		$this->papar->_jadual = $myTable;
+		//$this->papar->medan = array('website_name','website_link','note');
+	}
+#-------------------------------------------------------------------------------------------
 /*	public function staffAdd()
 	{
 		//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
@@ -86,10 +146,10 @@ class Vendor extends \Aplikasi\Kitab\Kawal
 	{
 		echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
 		# Set pembolehubah utama
-		//$this->panggilDB('login'); # panggil fungsi panggilDB
+		$this->panggilDB('login'); # panggil fungsi panggilDB
 
 		# Pergi papar kandungan
-		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
 		//$fail = array('jadual'); //$this->_folder = 'cari';
 		//$this->paparKandungan($this->_folder, $fail[0], $noInclude=1);
 	}
