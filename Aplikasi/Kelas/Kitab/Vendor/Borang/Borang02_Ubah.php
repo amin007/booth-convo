95feeec1722f6cb080975dc92e273610e2c97b74
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 15:31:56 +0800

    tambah koding baru untuk ubahInput2

--- Aplikasi/Kelas/Kitab/Vendor/Borang/Borang02_Ubah.php
+++ Aplikasi/Kelas/Kitab/Vendor/Borang/Borang02_Ubah.php
@@ -131,6 +131,58 @@ class Borang02_Ubah
 #------------------------------------------------------------------------------------------
 	public function ubahInput2($paparSahaja,$jadual,$kira,$key,$data)
 	{
+		# istihar pembolehubah
+		$name = 'name="' . $jadual . '[' . $key . ']"';
+		$dataType = myGetType($data);
+		# css
+		list($tab2,$tab3,$tab4,$birutua,$birumuda,$merah,
+			$classInput,$komenInput,$medanLogin) = $this->ccs();
+		$tab = array($tab2,$tab3,$tab4);
+
+		//if ( in_array($key,array(...)) )
+		if(in_array($key,array('nota','nota_prosesan','catatan','CatatNota')))
+			$input = $this->inputTextarea($tab2, $name, $data); #kod utk textarea
+		elseif(in_array($key,array('nota','nota_prosesan','catatan','CatatNota')))
+			$input = $this->inputTextarea($tab2, $name, $data); #kod utk textarea
+		elseif ( in_array($key,array('password','kataLaluan')) )
+			$input = $this->inputPassword($tab2, $tab3, $name, $data,
+				$classInput, $komenInput, $jadual, $key);
+		elseif(in_array($key,$medanLogin )) #senarai medan untuk login
+			$input = $this->inputBiodata($tab, $name, $data,
+				$classInput, $komenInput);
+		elseif ( in_array($key,array('keterangan')) ) # kod html untuk bukan input type
+			$input = $this->inputJadual($paparSahaja);
+		elseif(in_array($key,array('hasil','belanja','bilpekerja','gaji',
+			'hartatetap','stokakhir','staf','aset','stok')))
+			$input = $this->inputNumber($tab2, $tab2, $name, $data,
+				$classInput, $komenInput);
+		elseif ( in_array($key,array('lawat','terima','hantar','hantar_prosesan')) )
+			$input = $this->inputTarikh($tab2, $tab2, $name, $data,
+				$classInput, $komenInput, $jadual, $key);
+		elseif(in_array($key,array('no','batu','jalan','tmn_kg','daerah')))
+			$input = $this->inputAlamatBaru($tab2, $tab3, $name, $data,
+				$classInput, $komenInput);
+		elseif(in_array($key,array('namax','emailx','responden','fe',
+			'mko','respon','notel','nofax')))
+			$input = $this->inputTeksBesar($tab2, $tab3, $name, $data,
+				$classInput, $komenInput);
+		elseif(in_array($key,array('orang_a','notel_a','nofax_a','email_a')))
+			$input = $this->inputTeksBesar($tab2, $tab3, $name, $data,
+				$classInput, $komenInput);
+		elseif(in_array($key,array('pecah5P')))
+			$input = $this->inputTeksTakData($tab2, $tab3, $name);
+		elseif(in_array($key,array('state','negeri')))
+			$input = $this->inputDropmenuNg($tab2, $tab3, $name, $data,
+			$classInput, $komenInput, $key);
+		elseif(in_array($key,array('Action')))
+			$input = $this->inputButangAction($tab2, $tab3, $name, $data,
+			$classInput, $komenInput, $key);
+		else
+		{#kod untuk lain2
+			$input = $tab2 . '<p class="form-control-static text-info">'
+				   . $data . '</p>';
+		}
+
 		return $input; # pulangkan nilai
 	}
 #------------------------------------------------------------------------------------------
@@ -185,6 +237,7 @@ class Borang02_Ubah
 	}
 #------------------------------------------------------------------------------------------
 ###########################################################################################
+#------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 	function inputTextarea($tab2, $name, $data)
 	{
