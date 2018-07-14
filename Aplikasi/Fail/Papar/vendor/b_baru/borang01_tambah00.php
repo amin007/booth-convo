2648974d914fe42c2f56ddcf0e8438df37516b38
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 20:49:58 +0800

    tambah fail baru

---
+++ Aplikasi/Fail/Papar/vendor/b_baru/borang01_tambah00.php
@@ -0,0 +1,64 @@
+<center>
+<div class="row">
+
+<div class="col">
+	Product :
+	<span id="spryselect1">
+		<select  id="test" name="product" class="input">
+		<option value="" selected="selected">--- select product ---</option>
+		<option value="MY">Food & Drink</option>
+		<option value="JP">Flower</option>
+		<option value="JP">Clothes</option>
+		<option value="JP">Cosmetic</option>
+		</select>
+	</span>
+</div><!-- / class="col" -->
+
+<div class="col">
+	<div id="ic" style="display:none;">
+		SSM: 
+		<span id="sprytextfield2">
+			<input name="ssm" type="text" class="input" id="ssm" value="" size="20"/>
+		</span>
+	</div><!-- /  id="ic" style="display:none;" -->
+</div><!-- / class="col" -->
+<div id="passport" style="display:none;">
+	<div class="w-100">Lesen Berniaga : 
+		<span id="sprytextfield2">
+			<input name="ssm" type="text" class="input" id="ssm" value="" size="15"/>
+		</span>
+	</div><!-- / class="w-100 -->
+</div><!-- id="passport" style="display:none;" -->
+<div id="yuran" style="display:none;">
+	<div class="col">Yuran Proses : 
+		<span id="sprytextfield2">
+			<input name="ssm" type="text" class="input" id="ssm" value="" size="15"/>
+		</span>
+	</div><!-- / class="col" -->
+</div><!-- id="passport" style="display:none;" -->
+<div id="sijil" style="display:none;">
+		<div class="col">Sijil Pengendalian Makanan : 
+			<span id="sprytextfield2">
+				<input name="ssm" type="text" class="input" id="ssm" value="" size="5"/>
+			</span>
+		</div><!-- / class="col" -->
+</div><!-- id="passport" style="display:none;" -->
+<div id="suntikan" style="display:none;">
+	<div class="col">Suntikan Thypoid : 
+		<span id="sprytextfield2">
+			<input name="ssm" type="text" class="input" id="ssm" value="" size="15"/>
+		</span>
+	</div>
+</div><!-- id="suntikan" style="display:none;" -->
+</div></center>
+<!-- //banner -->
+<!-- gallery -->
+<table>
+<tr>
+<td align="right">&nbsp;</td>
+<td align="left"><br><br>
+	<button type="submit" name="login" class="butangadmin"><span>SUBMIT</span></button>
+	<button type="reset" class="butangadmin"><span>CLEAR</span></button>
+</td>
+</tr>
+</table> 
