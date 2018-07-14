c1dd46ed0aea49159422945d3dc99b6d0411ba86
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 15:40:20 +0800

    tukar $dataID kepada $pilih, tambah tatasusunan kepada $surat

--- Aplikasi/Kelas/Utama/Kawal/vendor.php
+++ Aplikasi/Kelas/Utama/Kawal/vendor.php
@@ -117,32 +117,32 @@ class Vendor extends \Aplikasi\Kitab\Kawal
 		//$this->papar->medan = array('website_name','website_link','note');
 	}
 #-------------------------------------------------------------------------------------------
-	public function updateID($dataID)
+	public function updateID($pilih)
 	{
 		# ubahsuai $posmen
-		//list($posmen,$senaraiJadual,$myTable,$medanID) = $this->ubahsuaiPost($dataID);
-		echo '<br>$dataID=' . $dataID . '<br>';
-		echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
+		list($posmen,$senaraiJadual,$myTable,$medanID) = $this->ubahsuaiPost($pilih);
+		//echo '<br>$dataID=' . $dataID . '<br>';
+		//echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
 		//echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
 
-		/*# mula ulang $senaraiJadual
+		# mula ulang $senaraiJadual
 		foreach ($senaraiJadual as $kunci => $jadual)
 		{# mula ulang table
-			$this->tanya->ubahSqlSimpan
-			//ubahSimpan
+			$this->tanya->//ubahSqlSimpan
+			ubahSimpan
 			($posmen[$jadual], $jadual, $medanID);
 		}# tamat ulang table
 
 		# pergi papar kandungan
-		$lokasi = 'borang/ubah/' . $myTable . '/' . $medanID . '/' . $dataID;
+		$lokasi = 'vendor/profile';
 		//echo '<br>location: ' . URL . $lokasi;
-		//header('location: ' . URL . $lokasi); //*/
+		header('location: ' . URL . $lokasi); //*/
 	}
 #-------------------------------------------------------------------------------------------
-	function ubahsuaiPost($dataID)
+	function ubahsuaiPost($pilih)
 	{
-		//list($medanID,$senaraiJadual,$pass,$pass2) = dpt_senarai('jadual_biodata3');
-		list($medanID,$senaraiJadual,$pass,$pass2) = $this->tanya->jadualLogin();
+		//list($medanID,$senaraiJadual,$pass,$pass2) = $this->tanya->pilihJadual('login');
+		list($senaraiJadual,$medanID) = $this->tanya->pilihJadual($pilih);
 
 		$posmen = array();
 		foreach ($_POST as $myTable => $value):
@@ -150,9 +150,9 @@ class Vendor extends \Aplikasi\Kitab\Kawal
 				foreach ($value as $kekunci => $papar)
 				{
 					$posmen[$myTable][$kekunci] = bersih($papar);
-					$posmen[$myTable][$medanID] = $dataID;
-				}//*/
-		endif; endforeach;
+					//$posmen[$myTable][$medanID] = $dataID;
+				}
+		endif; endforeach;//*/
 
 		//echo '<pre>$senaraiJadual='; print_r($senaraiJadual); echo '</pre>';
 		//echo '<pre>$medanID='; print_r($medanID); echo '</pre>';
@@ -160,21 +160,22 @@ class Vendor extends \Aplikasi\Kitab\Kawal
 		//echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
 
 		$posmen = $this->kataLaluanX($senaraiJadual[0], $posmen);
-		$posmen = $this->tanya->semakPosmen($senaraiJadual[0], $posmen, $pass, $pass2);
+		//$posmen = $this->tanya->semakPosmen($senaraiJadual[0], $posmen, $pass, $pass2);
 
 		return array($posmen,$senaraiJadual,$senaraiJadual[0],$medanID); # pulangkan nilai
 	}
 #-------------------------------------------------------------------------------------------
 	public function kataLaluanX($myTable, $posmen)
 	{
-		$surat = array('kataLaluan');
+		$surat = array('password','kataLaluan');
 		foreach ($surat as $kekunci)
 		if(isset($posmen[$myTable][$kekunci . 'X']))
 			if(empty($posmen[$myTable][$kekunci . 'X'])):
 				unset($posmen[$myTable][$kekunci . 'X']);
+				unset($posmen[$myTable][$kekunci . '']);
 			else:
 				unset($posmen[$myTable][$kekunci . 'X']);
-			endif;
+		endif;
 
 		return $posmen; # pulangkan nilai
 	}
