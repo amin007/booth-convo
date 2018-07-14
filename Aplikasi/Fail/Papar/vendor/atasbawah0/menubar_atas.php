d512746170a248cdd2c28d4cc4f242094e0bb2af
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 10:12:47 +0800

    tambah icon font awesome

--- Aplikasi/Fail/Papar/vendor/atasbawah0/menubar_atas.php
+++ Aplikasi/Fail/Papar/vendor/atasbawah0/menubar_atas.php
@@ -12,34 +12,19 @@ $iconFA['Power'] = '<i class="fas fa-power-off"></i>';
 	<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 		<ul class="navbar-nav mr-auto">
 		<li class="nav-item">
-			<a class="nav-link" href="<?php echo URL ?>vendor/profile">profile</a>
+			<a class="nav-link" href="<?php echo URL ?>vendor/profile">
+			<?php echo $iconFA['User'] ?>profile</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" href="<?php echo URL ?>vendor/booking">booking</a>
+			<a class="nav-link" href="<?php echo URL ?>vendor/booking">
+			<?php echo $iconFA['Ask'] ?>booking</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" href="<?php echo URL ?>vendor/status">status</a>
+			<a class="nav-link" href="<?php echo URL ?>vendor/status">
+			<?php echo $iconFA['Barcode'] ?>status</a>
 		</li>
 		<li class="nav-item">
 			<a class="nav-link" href="<?php echo URL ?>vendor/payment">payment</a>
-		</li>
-		<li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
-			aria-haspopup="true" aria-expanded="false">Dropdown</a>
-			<div class="dropdown-menu" aria-labelledby="dropdown01">
-			<a class="dropdown-item" href="#">Action</a>
-			<a class="dropdown-item" href="#">Another action</a>
-			<a class="dropdown-item" href="#">Something else here</a>
-			</div>
-		</li>
-		<li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
-			aria-haspopup="true" aria-expanded="false">Dropdown</a>
-			<div class="dropdown-menu" aria-labelledby="dropdown01">
-			<a class="dropdown-item" href="#">Action</a>
-			<a class="dropdown-item" href="#">Another action</a>
-			<a class="dropdown-item" href="#">Something else here</a>
-			</div>
 		</li>
 		</ul>
 	</div>
