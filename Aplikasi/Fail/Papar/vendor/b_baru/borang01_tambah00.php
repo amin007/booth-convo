7f60defdb526c2997eea69ca2ca4f910c1572480
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 23:54:32 +0800

    susun borang ikut yang asal dan baru

--- Aplikasi/Fail/Papar/vendor/b_baru/borang01_tambah00.php
+++ Aplikasi/Fail/Papar/vendor/b_baru/borang01_tambah00.php
@@ -13,7 +13,28 @@
 		</span>
 	</div><!-- / class="col" -->
 </div><!-- / class="row" -->
-<div class="row">
+
+<?php
+?>
+
+</center>
+
+<table>
+<tr>
+<td align="right">&nbsp;</td>
+<td align="left"><br><br>
+	<button type="submit" name="login" class="butangadmin"><span>SUBMIT</span></button>
+	<button type="reset" class="butangadmin"><span>CLEAR</span></button>
+</td>
+</tr>
+</table>
+
+<br><br><hr>
+<?php
+#---------------------------------------------------------------------------------------------
+function input_asal()
+{
+?><div class="row">
 	<div class="col" id="ic" style="display:none;">
 		SSM:
 		<span id="sprytextfield2">
@@ -52,45 +73,40 @@
 			<input name="ssm" type="text" class="input" id="ssm" value="" size="15"/>
 		</span>
 	</div><!-- / class="col"id="suntikan" style="display:none;" -->
-</div><!-- / class="row" -->
-
-<table>
-<tr>
-<td align="right">&nbsp;</td>
-<td align="left"><br><br>
-	<button type="submit" name="login" class="butangadmin"><span>SUBMIT</span></button>
-	<button type="reset" class="butangadmin"><span>CLEAR</span></button>
-</td>
-</tr>
-</table> 
-
-<br><br><hr>
-<form align="center">
+</div><!-- / class="row" --><?php
+}
+#---------------------------------------------------------------------------------------------
+function input_baru()
+{?>
+	<form align="center">
+	<?php
+	$class2 = 'col-sm-7'; # untuk $data
+	// ic,passport,yuran,sijil,suntikan
+	$ulangan = array('ic'=>'SSM','passport'=>'Lesen Berniaga',
+	'yuran'=>'Yuran Proses',
+	'sijil'=>'Sijil Pengendalian Makanan',
+	'suntikan'=>'Suntikan Thypoid');
+	foreach($ulangan as $label => $input):
+	?>
+	<div class="form-group row">
+		<label for="inputTajuk" style="text-align:left"
+		class="col-sm-3 control-label"><?php
+		echo $input ?></label>
+		<div class="<?php echo $class2 ?>" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"
+					id="sprytextfield2"><?php
+					echo $label ?></span>
+				</div>
+				<input type="text" class="form-control">
+			</div><!-- /  class="input-group" -->
+		</div><!-- / class="<?php echo $class2 ?>" -->
+	</div><!-- / class="form-group row" -->
+	<?php
+	endforeach;
+	?>
+	</form>
 <?php
-$class2 = 'col-sm-7'; # untuk $data
-// ic,passport,yuran,sijil,suntikan
-$ulangan = array('ic'=>'SSM','passport'=>'Lesen Berniaga',
-'yuran'=>'Yuran Proses',
-'sijil'=>'Sijil Pengendalian Makanan',
-'suntikan'=>'Suntikan Thypoid');
-foreach($ulangan as $label => $input):
-?>
-<div class="form-group row">
-	<label for="inputTajuk" style="text-align:left"
-	class="col-sm-3 control-label"><?php
-	echo $input ?></label>
-	<div class="<?php echo $class2 ?>" >
-		<div class="input-group">
-			<div class="input-group-prepend">
-				<span class="input-group-text"
-				id="sprytextfield2"><?php
-				echo $label ?></span>
-			</div>
-			<input type="text" class="form-control">
-		</div><!-- /  class="input-group" -->
-	</div><!-- / class="<?php echo $class2 ?>" -->
-</div><!-- / class="form-group row" -->
-<?php
-endforeach;
-?>
-</form>
+}
+#---------------------------------------------------------------------------------------------
