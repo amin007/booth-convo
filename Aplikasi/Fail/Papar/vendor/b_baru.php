0bbef37e681681bf045178bff09d4799fa180139
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-14 15:40:20 +0800

    tambah tajuk baru

--- Aplikasi/Fail/Papar/vendor/b_baru.php
+++ Aplikasi/Fail/Papar/vendor/b_baru.php
@@ -17,7 +17,8 @@ include 'atasbawah0/menu_atas.php';
 <div class="row">
 <div class="col-md-12">
 <?php
-//include 'jadual/pilih_a_tajuk.php';
+$tajukjadual = 'Ubah Data';
+include 'jadual/pilih_a_tajuk.php';
 include 'b_baru/b02_ulangjadual.php';
 echo "\n\n";
 ?><!-- / class="card" -->
