2103385cf15f72561058673f6525d97f018502c6
Amin007 Ledang <helmi007@yahoo.com>
2018-07-15 11:35:55 +0800

    panggil 2 jadual

--- Aplikasi/Kelas/Utama/Kawal/vendor.php
+++ Aplikasi/Kelas/Utama/Kawal/vendor.php
@@ -294,6 +294,7 @@ class Vendor extends \Aplikasi\Kitab\Kawal
 		//echo '<hr>Nama class :' . __METHOD__ . '()<hr>';
 		# Set pembolehubah utama
 		$this->panggilDB('status'); # panggil fungsi panggilDB
+		$this->panggilDB('booking'); # panggil fungsi panggilDB
 		//$this->debugKandunganPaparan();# Semak data dulu
 
 		# Pergi papar kandungan
