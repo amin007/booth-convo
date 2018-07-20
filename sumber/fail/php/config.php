add3dbb9614c3d630771fa423a897719cbf10d23
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-20 12:20:52 +0800

    tukar $this->db kepada $connect

--- sumber/fail/php/config.php
+++ sumber/fail/php/config.php
@@ -1,4 +1,4 @@
 <?php
 require '../../../tatarajah.php'; # rujuk contoh i-tatarajah.php
 require '../../../Aplikasi/Kelas/Kitab/DB/DB_Pdo.php';
-$this->db = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+$connect = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
