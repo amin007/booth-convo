45db71cfdcb71fbbc39fcd3347413a1bdb87a1d6
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 10:15:34 +0800

    semak data $_GET

--- sumber/fail/php2/papar_tatasusunan.php
+++ sumber/fail/php2/papar_tatasusunan.php
@@ -1,5 +1,6 @@
-	<tr><td colspan="14">
+	<tr><td colspan="8">
 <?php
+echo '<pre>$_GET:'; print_r($_GET); echo '</pre><hr>';
 echo '<pre>$_POST:'; print_r($_POST); echo '</pre><hr>';
 echo '<pre>$data:'; print_r($data); echo '</pre><hr>';
 
