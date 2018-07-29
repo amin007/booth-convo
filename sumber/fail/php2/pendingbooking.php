aa132cdf43dacaf55d35ca98548b6c09fa154ec6
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 11:50:56 +0800

    tukar $myTable kepada $ceklist

--- sumber/fail/php2/pendingbooking.php
+++ sumber/fail/php2/pendingbooking.php
@@ -5,9 +5,9 @@ include('config.php');
 //echo '<pre>$_POST:'; print_r($_POST); echo '</pre><hr>';
 #----------------------------------------------------------------------------------------------------
 # setkan nilai awal
-$myTable = 'test3_criteria';
+$ceklist = 'semak';
 # godek data $_POST
-list($posmen,$myTable) = ubahsuaiPost($myTable);
+list($posmen,$myTable) = ubahsuaiPost($ceklist);
 #----------------------------------------------------------------------------------------------------
 # proses debug
 echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
@@ -25,21 +25,21 @@ if($ssm=="yes" && $datessm=="yes" && $lesenberniaga=="yes" && $datelesenberniaga
 	//"UPDATE criteria SET status = 'accepted' WHERE ssm = $ssm");
 */
 #----------------------------------------------------------------------------------------------------
-	function ubahsuaiPost($myTable)
+	function ubahsuaiPost($ceklist)
 	{
 		$posmen = array();
 		foreach($_POST as $jadual => $k1):
 			if (is_array($k1) || is_object($k1))
 			{# mula - pastikan $k1 wujud
 				foreach($k1 as $kekunci => $papar):
-					if($jadual == $myTable)
-						$posmen[$myTable][$kekunci] = bersih($papar);
+					if($jadual == $ceklist)
+						$posmen[$ceklist][$kekunci] = bersih($papar);
 				endforeach;
 			}# tamat - pastikan $_POST wujud
 		endforeach;
 		//echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
 		//echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
 
-		return array($posmen,$myTable); # pulangkan nilai
+		return array($posmen,$ceklist); # pulangkan nilai
 	}
 #----------------------------------------------------------------------------------------------------
