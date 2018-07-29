da34e3b71c3c71862b7d5ea37bc5851338bed137
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 08:38:40 +0800

    sambungan ke databasew menggunakan mysqli

--- sumber/fail/php2/config.php
+++ sumber/fail/php2/config.php
@@ -7,5 +7,12 @@ date_default_timezone_set('Asia/Kuala_Lumpur');
  
 # 3. setkan tatarajah sistem
 require '../../../tatarajah.php'; # rujuk contoh i-tatarajah.php
-require '../../../Aplikasi/Kelas/Kitab/DB/DB_Pdo.php';
-$connect = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+//require '../../../Aplikasi/Kelas/Kitab/DB/DB_Pdo.php';
+//$connect = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+
+$connect = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if($connect->connect_errno):
+	echo 'Failed to connect to MySQL : ' . $connect->error;
+else:
+	//echo '<h1>Berjaya masuk ke database<hr>';
+endif;
