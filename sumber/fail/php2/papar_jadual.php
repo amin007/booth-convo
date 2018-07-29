70c9ea4b4f3af8a77ced45b4c1bed9b4ad005603
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 09:20:33 +0800

    tukar 11 kepada sijilpmakanan

--- sumber/fail/php2/papar_jadual.php
+++ sumber/fail/php2/papar_jadual.php
@@ -27,7 +27,7 @@
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align='center'><?php echo $row['11'] ?></td></center>
+	<td align='center'><?php echo $row['sijilpmakanan'] ?></td></center>
 	<td align="center">
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
