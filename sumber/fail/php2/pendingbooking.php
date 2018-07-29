bf9930425c4961be7c87af9ec32701c3a0a0ad20
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 11:38:23 +0800

    debug sama ada $k1 adalah tatasusunan

--- sumber/fail/php2/pendingbooking.php
+++ sumber/fail/php2/pendingbooking.php
@@ -10,10 +10,10 @@ $myTable = 'test3_criteria';
 list($posmen,$myTable) = ubahsuaiPost($myTable);
 #----------------------------------------------------------------------------------------------------
 # proses debug
-//echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
-//echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
+echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
+echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
 #----------------------------------------------------------------------------------------------------
-//$ssm = $datessm = $lesenberniaga = $datelesenberniaga = $suntikan = $sijilpmakanan = 'yes';
+$ssm = $datessm = $lesenberniaga = $datelesenberniaga = $suntikan = $sijilpmakanan = 'yes';
 #----------------------------------------------------------------------------------------------------
 if($ssm=="yes" && $datessm=="yes" && $lesenberniaga=="yes" && $datelesenberniaga=="yes"
 && $suntikan=="yes" && $sijilpmakanan=="yes")
@@ -29,8 +29,14 @@ if($ssm=="yes" && $datessm=="yes" && $lesenberniaga=="yes" && $datelesenberniaga
 	function ubahsuaiPost($myTable)
 	{
 		$posmen = array();
-		foreach ($_POST as $kekunci => $papar):
-			$posmen[$myTable][$kekunci] = bersih($papar);
+		foreach($_POST as $jadual => $k1):
+			if (is_array($k1) || is_object($k1))
+			{# mula - pastikan $k1 wujud
+				foreach($k1 as $kekunci => $papar):
+					if($jadual == $myTable)
+						$posmen[$myTable][$kekunci] = bersih($papar);
+				endforeach;
+			}# tamat - pastikan $_POST wujud
 		endforeach;
 		//echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
 		//echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
