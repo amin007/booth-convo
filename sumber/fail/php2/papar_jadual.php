a80b6947b380dcd8235fc47367ae0b568c4a851f
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 09:57:55 +0800

    kurangkan penggunaan <td>

--- sumber/fail/php2/papar_jadual.php
+++ sumber/fail/php2/papar_jadual.php
@@ -1,53 +1,34 @@
-	<tr >
+	<tr>
 	<td align="center"><?php echo $row['idvendor'] ?></td>
-	<td align="center"><?php echo $row['ssm'] ?></td>
-	<td align="center">
+	<td align="center"><?php echo $row['ssm'] ?><br>
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align="center"><?php echo $row['datessm'] ?></td>
-	<td align="center">
+	<td align="center"><?php echo $row['datessm'] ?><br>
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align="center"><?php echo $row['lesenberniaga'] ?></td>
-	<td align="center">
+	<td align="center"><?php echo $row['lesenberniaga'] ?><br>
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align="center"><?php echo $row['datelesenberniaga'] ?></td>
-	<td align="center">
+	<td align="center"><?php echo $row['datelesenberniaga'] ?><br>
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align="center"><?php echo $row['suntikan'] ?></td>
-	<td align="center">
+	<td align="center"><?php echo $row['suntikan'] ?><br>
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align="center"><?php echo $row['sijilpmakanan'] ?></td>
-	<td align="center">
+	<td align="center"><?php echo $row['sijilpmakanan'] ?><br>
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<!--<td align="center"></td></center>-->
 	<td>
-<?php
-	if($ssm=="yes" && $datessm=="yes" && $lesenberniaga=="yes"
-	&& $datelesenberniaga=="yes" 
-	&& $suntikan=="yes" && $sijilpmakanan=="yes")
-	{
-		//echo '$status';
-	}//*/
-?>
 		<form action="pendingbooking.php" method="post">
 			<span>Status = <?php echo $row['status'] ?></span><hr>
 			<button class="btn success-btn" name="accepted"
 			value="accepted">Approve</button>
 		</form>
-<?php
-	//echo $select  = mysqli_query($connect, 
-	//"UPDATE criteria SET status = 'accepted' WHERE ssm = $ssm");
-?>
 	</td>
 	</tr>
