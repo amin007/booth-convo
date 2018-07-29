186f0d1ec535de1b4527e45c8d1e483a1012b7e6
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 09:21:42 +0800

    buang </center>

--- sumber/fail/php2/papar_jadual.php
+++ sumber/fail/php2/papar_jadual.php
@@ -17,17 +17,17 @@
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align='center'><?php echo $row['datelesenberniaga'] ?></td></center>
+	<td align='center'><?php echo $row['datelesenberniaga'] ?></td>
 	<td align="center">
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align='center'><?php echo $row['suntikan'] ?></td></center>
+	<td align='center'><?php echo $row['suntikan'] ?></td>
 	<td align="center">
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
 	</td>
-	<td align='center'><?php echo $row['sijilpmakanan'] ?></td></center>
+	<td align='center'><?php echo $row['sijilpmakanan'] ?></td>
 	<td align="center">
 		<input type="checkbox" name="" value="yes"> Yes<br>
 		<input type="checkbox" name="" value="no"> No
