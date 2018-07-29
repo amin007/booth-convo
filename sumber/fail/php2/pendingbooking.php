242c837e090248cd7d79bd1e05fb13789e200c28
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-07-29 11:55:34 +0800

    bentuk 2 tatasusunan $posmen dan $posmen2

--- sumber/fail/php2/pendingbooking.php
+++ sumber/fail/php2/pendingbooking.php
@@ -7,11 +7,12 @@ include('config.php');
 # setkan nilai awal
 $ceklist = 'semak';
 # godek data $_POST
-list($posmen,$myTable) = ubahsuaiPost($ceklist);
+list($posmen,$posmen2) = ubahsuaiPost($ceklist);
 #----------------------------------------------------------------------------------------------------
 # proses debug
 echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
 echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
+echo '<pre>$posmen2='; print_r($posmen2); echo '</pre>';
 #----------------------------------------------------------------------------------------------------
 /*$ssm = $datessm = $lesenberniaga = $datelesenberniaga = $suntikan = $sijilpmakanan = 'yes';
 if($ssm=="yes" && $datessm=="yes" && $lesenberniaga=="yes" && $datelesenberniaga=="yes"
@@ -27,19 +28,21 @@ if($ssm=="yes" && $datessm=="yes" && $lesenberniaga=="yes" && $datelesenberniaga
 #----------------------------------------------------------------------------------------------------
 	function ubahsuaiPost($ceklist)
 	{
-		$posmen = array();
+		$posmen = $posmen2 = array();
 		foreach($_POST as $jadual => $k1):
-			if (is_array($k1) || is_object($k1))
-			{# mula - pastikan $k1 wujud
+			if (is_array($k1) || is_object($k1)):
+			# mula - pastikan $k1 wujud
 				foreach($k1 as $kekunci => $papar):
 					if($jadual == $ceklist)
-						$posmen[$ceklist][$kekunci] = bersih($papar);
+						$posmen[$kekunci] = bersih($papar);
 				endforeach;
-			}# tamat - pastikan $_POST wujud
+			else:
+				$posmen2[$jadual] = bersih($k1);
+			endif;
 		endforeach;
 		//echo '<pre>$_POST='; print_r($_POST); echo '</pre>';
 		//echo '<pre>$posmen='; print_r($posmen); echo '</pre>';
 
-		return array($posmen,$ceklist); # pulangkan nilai
+		return array($posmen,$posmen2); # pulangkan nilai
 	}
 #----------------------------------------------------------------------------------------------------
